391b60e6dc8c7afef8207c75991f7bc4504e8079
8blank8 <springfield.3298@gmail.com>
2023-05-03 18:10:31 +0700

    add message validation

--- src/middlewares/input-validation-middleware.ts
+++ src/middlewares/input-validation-middleware.ts
@@ -5,7 +5,9 @@ import { validationResult, body } from "express-validator"
 export const inputValidationMiddleware = (req: Request, res: Response, next: NextFunction)=>{
     const errors = validationResult(req)
     if(!errors.isEmpty()){
-       res.status(400).send({errors: errors.array()})
+      
+      const err = errors.array().map(item => ({message: item.msg, field: item.path}))
+       res.status(400).send({errors:  err})
     }else{
        next()
     }
--- src/validations/validations-video.ts
+++ src/validations/validations-video.ts
@@ -3,18 +3,18 @@ import { inputValidationMiddleware } from "../middlewares/input-validation-middl
 
 
 export const validationCreateVideo = [
-    body('title').trim().notEmpty().isLength({min: 1, max: 40}),
-    body('author').trim().notEmpty().isLength({min: 1, max: 20}),
-    body('availableResolutions').notEmpty().isArray().isIn(["P144", "P240", "P360", "P480", "P720", "P1080", "P1440", "P2160"]),
+    body('title').trim().notEmpty().isLength({min: 1, max: 40}).withMessage('title length min 1 max 40'),
+    body('author').trim().notEmpty().isLength({min: 1, max: 20}).withMessage('author length min 1 max 20'),
+    body('availableResolutions').notEmpty().isArray().isIn(["P144", "P240", "P360", "P480", "P720", "P1080", "P1440", "P2160"]).withMessage('availableResolutions has been value ["P144", "P240", "P360", "P480", "P720", "P1080", "P1440", "P2160"]'),
     inputValidationMiddleware
 ]
 
 export const validationUpdateVideo = [
-    body('title').trim().notEmpty().isLength({min: 1, max: 40}),
-    body('author').trim().notEmpty().isLength({min: 1, max: 20}),
-    body('availableResolutions').notEmpty().isArray().isIn(["P144", "P240", "P360", "P480", "P720", "P1080", "P1440", "P2160"]),
-    body('canBeDownloaded').notEmpty().isIn([true, false]),
-    body('minAgeRestriction').custom(item => item === null || item > 1 && item < 19),
-    body('publicationDate').notEmpty().isString(),
+    body('title').trim().notEmpty().isLength({min: 1, max: 40}).withMessage('title length min 1 max 40'),
+    body('author').trim().notEmpty().isLength({min: 1, max: 20}).withMessage('author length min 1 max 20'),
+    body('availableResolutions').notEmpty().isArray().isIn(["P144", "P240", "P360", "P480", "P720", "P1080", "P1440", "P2160"]).withMessage('availableResolutions has been value ["P144", "P240", "P360", "P480", "P720", "P1080", "P1440", "P2160"]'),
+    body('canBeDownloaded').notEmpty().isIn([true, false]).withMessage('canBeDownloaded has been value true or false'),
+    body('minAgeRestriction').custom(item => item === null || item > 1 && item < 19).withMessage('minAgeRestriction has been value nul or 1 - 18'),
+    body('publicationDate').notEmpty().isString().withMessage('publicationDate has been type string'),
     inputValidationMiddleware
 ]
