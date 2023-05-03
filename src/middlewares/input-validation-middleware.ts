266042ebd0a936260026618a5379f24ad67ba6b4
8blank8 <springfield.3298@gmail.com>
2023-05-03 22:21:51 +0700

    add authorization

--- src/middlewares/input-validation-middleware.ts
+++ src/middlewares/input-validation-middleware.ts
@@ -1,12 +1,12 @@
 import { NextFunction, Request, Response } from "express"
-import { validationResult, body } from "express-validator"
+import { validationResult } from "express-validator"
 
 
 export const inputValidationMiddleware = (req: Request, res: Response, next: NextFunction)=>{
+
     const errors = validationResult(req)
     if(!errors.isEmpty()){
-      
-      const err = errors.array().map(item => ({message: item.msg, field: item.path}))
+      const err = errors.array({onlyFirstError: true}).map(item => ({message: item.msg, field: item.path}))
        res.status(400).send({errorsMessages:  err})
     }else{
        next()
--- src/validations/validations-posts.ts
+++ src/validations/validations-posts.ts
@@ -1,7 +1,22 @@
 import { inputValidationMiddleware } from "../middlewares/input-validation-middleware";
+import { Request, Response } from "express";
 import { body } from "express-validator";
+import { blogsRepository } from "../repositories/blogs-repository";
 
 export const validationCreateOrUpdatePost = [
+    body('title').notEmpty().withMessage('title is required'),
+    body('title').isString().isLength({min: 1, max: 30}).withMessage('title length min 1 max 30'),
+    body('shortDescription').notEmpty().withMessage('shortDescription is required'),
+    body('shortDescription').isString().isLength({min: 1, max: 100}).withMessage('shortDescription length min 1 max 100'),
+    body('content').notEmpty().withMessage('content is required'),
+    body('content').isString().isLength({min: 1, max: 1000}).withMessage('content length min 1 max 1000'),
+    body('blogId').notEmpty().withMessage('blogId is required'),
+    body('blogId').isString().withMessage('blogId must be string'),
+    body('blogId').custom(id=> blogsRepository.findBlogsById(id) !== undefined),
+    inputValidationMiddleware
+]
+
+export const validationUpdatePost = [
     body('title').notEmpty().withMessage('title is required'),
     body('title').isString().isLength({min: 1, max: 30}).withMessage('title length min 1 max 30'),
     body('shortDescription').notEmpty().withMessage('shortDescription is required'),
